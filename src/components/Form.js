9996aeff4a44c8405642f6be455c7d5fbf8770ca
aswathyeg <aswathy.eg.bijesh@gmail.com>
2023-06-26 13:56:37 +0530

    added useState

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,13 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Form = () => {
+  // let [current, setCurrent] = useState(0);
+  // const [yearly, setYearly] = useState(0);
+  // const [expected, setExpected] = useState(0);
+  // const [duration, setDuration] = useState(0);
+  const initialInput = {
+    "current-savings": 10000,
+    "yearly-contribution": 1200,
+    "expected-return": 7,
+    duration: 10,
+  };
+  const [userInput, setUserInput] = useState(initialInput);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log("submit");
+  };
+  const resetHandler = () => {
+    setUserInput(initialInput);
+  };
+
+  const inputChangeHandler = (input, value) => {
+    setUserInput((prevValue) => {
+      return {
+        ...prevValue,
+        [input]: +value,
+      };
+    });
+  };
   return (
     <div>
-      <form className="form">
+      <form className="form" onSubmit={submitHandler}>
         <div className="input-group">
           <p>
             <label htmlFor="current-savings">Current Savings ($)</label>
-            <input type="number" id="current-savings" />
+            <input
+              type="number"
+              id="current-savings"
+              onChange={(e) => {
+                inputChangeHandler("current-savings", e.target.value);
+              }}
+              value={userInput["current-savings"]}
+            />
           </p>
           <p>
             <label htmlFor="yearly-contribution">Yearly Savings ($)</label>
@@ -27,7 +62,7 @@ const Form = () => {
           </p>
         </div>
         <p className="actions">
-          <button type="reset" className="buttonAlt">
+          <button type="reset" className="buttonAlt" onClick={resetHandler}>
             Reset
           </button>
           <button type="submit" className="button">
