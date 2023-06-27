ed19274e4f104ce4b760288b431174f879e5c407
aswathyeg <aswathy.eg.bijesh@gmail.com>
2023-06-27 11:36:36 +0530

    added condition

--- src/App.js
+++ src/App.js
@@ -5,12 +5,15 @@ import OutputTable from "./components/OutputTable";
 
 function App() {
   const [userInput, setUserInput] = useState(null);
+
   const calculateHandler = (userInput) => {
-    // Should be triggered when form is submitted
-    // You might not directly want to bind it to the submit event on the form though...
+    setUserInput(userInput);
+  };
+  // Should be triggered when form is submitted
+  // You might not directly want to bind it to the submit event on the form though...
 
-    const yearlyData = []; // per-year results
-
+  const yearlyData = []; // per-year results
+  if (userInput) {
     let currentSavings = +userInput["current-savings"]; // feel free to change the shape of this input object!
     const yearlyContribution = +userInput["yearly-contribution"]; // as mentioned: feel free to change the shape...
     const expectedReturn = +userInput["expected-return"] / 100;
@@ -28,9 +31,8 @@ function App() {
         yearlyContribution: yearlyContribution,
       });
     }
-
     // do something with yearlyData ...
-  };
+  }
 
   return (
     <div>
